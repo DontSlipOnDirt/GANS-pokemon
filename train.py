97316c8fc1797b647b6f05733ad5dd312e11823d
Miguel Samaniego <ma.samaniego@hotmail.com>
2024-05-20 16:23:50 +0200

    Add training loop

--- train.py
+++ train.py
@@ -1,9 +1,13 @@
 import random
 import torch
 from torch import nn
+from torch import optim
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 from models import Generator, Discriminator
+import matplotlib.pyplot as plt
+import torchvision.utils as vutils
+import numpy as np
 
 # Set random seed for reproducibility
 manualSeed = 999
@@ -11,7 +15,7 @@ manualSeed = 999
 print("Random Seed: ", manualSeed)
 random.seed(manualSeed)
 torch.manual_seed(manualSeed)
-torch.use_deterministic_algorithms(True) # Needed for reproducible results
+# torch.use_deterministic_algorithms(True) # Needed for reproducible results
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def get_dataset(folder, image_size, batch_size):
@@ -35,13 +39,135 @@ def weights_init(m):
         nn.init.constant_(m.bias.data, 0)
 
 if __name__ == "__main__":
-    dataloader = get_dataset("pokemon_images/", 128, 128)
-    latent_dim = 100
+    # Read this from hyps file later
+    nz = 100
+    lr = 0.0002
+    beta1 = 0.5
+    num_epochs = 100
+    image_size = 64
+    batch_size = 128
+    # Load the dataset
+    dataloader = get_dataset("pokemon_images/", image_size, batch_size)
     # Create models
-    generator = Generator(latent_dim).to(device)
-    discriminator = Discriminator().to(device)
+    G = Generator().to(device)
+    D = Discriminator().to(device)
     # Initialize weights
-    generator.apply(weights_init)
-    discriminator.apply(weights_init)
-    print(generator)
-    print(discriminator)
+    G.apply(weights_init)
+    D.apply(weights_init)
+    # Initialize the ``BCELoss`` function
+    criterion = nn.BCELoss()
+
+    # Create batch of latent vectors that we will use to visualize
+    #  the progression of the generator
+    fixed_noise = torch.randn(64, nz, 1, 1, device=device)
+
+    # Establish convention for real and fake labels during training
+    real_label = 1.
+    fake_label = 0.
+
+    # Setup Adam optimizers for both G and D
+    D_optim = optim.Adam(D.parameters(), lr=lr, betas=(beta1, 0.999))
+    G_optim = optim.Adam(G.parameters(), lr=lr, betas=(beta1, 0.999))
+
+    img_list = []
+    G_losses = []
+    D_losses = []
+    iters = 0
+
+    print("Starting Training Loop...")
+    # For each epoch
+    for epoch in range(num_epochs):
+        # For each batch in the dataloader
+        for i, data in enumerate(dataloader, 0):
+
+            ############################
+            # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
+            ###########################
+            ## Train with all-real batch
+            D.zero_grad()
+            # Format batch
+            real_cpu = data[0].to(device)
+            b_size = real_cpu.size(0)
+            label = torch.full((b_size,), real_label, dtype=torch.float, device=device)
+            # Forward pass real batch through D
+            output = D(real_cpu).view(-1)
+            # Calculate loss on all-real batch
+            D_error_real = criterion(output, label)
+            # Calculate gradients for D in backward pass
+            D_error_real.backward()
+            D_x = output.mean().item()
+
+            ## Train with all-fake batch
+            # Generate batch of latent vectors
+            noise = torch.randn(b_size, nz, 1, 1, device=device)
+            # Generate fake image batch with G
+            fake = G(noise)
+            label.fill_(fake_label)
+            # Classify all fake batch with D
+            output = D(fake.detach()).view(-1)
+            # Calculate D's loss on the all-fake batch
+            D_error_fake = criterion(output, label)
+            # Calculate the gradients for this batch, accumulated (summed) with previous gradients
+            D_error_fake.backward()
+            D_G_z1 = output.mean().item()
+            # Compute error of D as sum over the fake and the real batches
+            D_error = D_error_real + D_error_fake
+            # Update D
+            D_optim.step()
+
+            ############################
+            # (2) Update G network: maximize log(D(G(z)))
+            ###########################
+            G.zero_grad()
+            label.fill_(real_label)  # fake labels are real for generator cost
+            # Since we just updated D, perform another forward pass of all-fake batch through D
+            output = D(fake).view(-1)
+            # Calculate G's loss based on this output
+            G_error = criterion(output, label)
+            # Calculate gradients for G
+            G_error.backward()
+            D_G_z2 = output.mean().item()
+            # Update G
+            G_optim.step()
+
+            # Output training stats
+            print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
+                % (epoch + 1, num_epochs, i + 1, len(dataloader),
+                    D_error.item(), G_error.item(), D_x, D_G_z1, D_G_z2))
+
+            # Save Losses for plotting later
+            G_losses.append(G_error.item())
+            D_losses.append(D_error.item())
+
+            # Check how the generator is doing by saving G's output on fixed_noise
+            if (iters % 500 == 0) or ((epoch == num_epochs-1) and (i == len(dataloader)-1)):
+                with torch.no_grad():
+                    fake = G(fixed_noise).detach().cpu()
+                img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
+            iters += 1
+
+    plt.figure(figsize=(10,5))
+    plt.title("Generator and Discriminator Loss During Training")
+    plt.plot(G_losses,label="G")
+    plt.plot(D_losses,label="D")
+    plt.xlabel("iterations")
+    plt.ylabel("Loss")
+    plt.legend()
+    plt.show()
+
+    # Grab a batch of real images from the dataloader
+    real_batch = next(iter(dataloader))
+
+    # Plot the real images
+    plt.figure(figsize=(15,15))
+    plt.subplot(1,2,1)
+    plt.axis("off")
+    plt.title("Real Images")
+    plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=5, normalize=True).cpu(),(1,2,0)))
+
+    # Plot the fake images from the last epoch
+    plt.subplot(1,2,2)
+    plt.axis("off")
+    plt.title("Fake Images")
+    plt.imshow(np.transpose(img_list[-1],(1,2,0)))
+    plt.show()
